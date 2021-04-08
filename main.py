4e66bc824748c1162f438302a3d7e651be85595f
Akash Prasad <40398087+akashpower1@users.noreply.github.com>
2021-04-08 20:31:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import connect
 import aggegrate as ag
-#Read the csv file from working directory
-products = pd.read_csv('products.csv')
 
-#Connect to Mysql database
-db = connect.DB_Connection()
-cnx,error = db.Mysqlconn()
+if __name__ == "__main__" :
+    # Read the csv file from working directory
+    products = pd.read_csv('products.csv')
 
-#If connection is succcessful then move forward
-if error == 0:
-    try:
-        print('Inserting into database.......')
-        products.to_sql('prod', cnx, if_exists='replace', index = False)
-        print('Insertion completed successfully!!')
+    # Connect to Mysql database
+    db = connect.DB_Connection()
+    cnx, error = db.Mysqlconn()
 
-        #aggegration of data according to name
-        aggegrate_data = ag.aggregated(cnx)
-        aggegrate_data.aggregated_prod(products)
-    except Exception as e:
-        print(e)
-        print('Insertion failed!!')
-    cnx.close()
+    # If connection is succcessful then move forward
+    if error == 0:
+        try:
+            print('Inserting into database.......')
+            products.to_sql('prod', cnx, if_exists='replace', index=False)
+            print('Insertion completed successfully!!')
+
+            # aggegration of data according to name
+            aggegrate_data = ag.aggregated(cnx)
+            aggegrate_data.aggregated_prod(products)
+        except Exception as e:
+            print(e)
+            print('Insertion failed!!')
+        cnx.close()
