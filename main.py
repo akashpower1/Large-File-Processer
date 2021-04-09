17886ad8c533f93c8616d932a29d776ff9854bf0
Akash Prasad <40398087+akashpower1@users.noreply.github.com>
2021-04-09 10:52:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import pandas as pd
 import connect
 import aggegrate as ag
+from config import chunk_size
 
-if __name__ == "__main__" :
-    # Read the csv file from working directory
-    products = pd.read_csv('products.csv')
+#Connect to Mysql database
+db = connect.DB_Connection()
+cnx,error = db.Mysqlconn()
 
-    # Connect to Mysql database
-    db = connect.DB_Connection()
-    cnx, error = db.Mysqlconn()
-
+if __name__ == "__main__":
     # If connection is succcessful then move forward
     if error == 0:
         try:
-            print('Inserting into database.......')
-            products.to_sql('prod', cnx, if_exists='replace', index=False)
+            cnx.execute('Truncate table prod;')
+
+            # Read the csv file from working directory
+            for chunk in pd.read_csv('products.csv', chunksize=chunk_size ):
+                print('Inserting into database.......')
+                chunk.to_sql('prod', cnx, if_exists='append', index=False)
             print('Insertion completed successfully!!')
 
             # aggegration of data according to name
             aggegrate_data = ag.aggregated(cnx)
-            aggegrate_data.aggregated_prod(products)
+            aggegrate_data.aggregated_prod()
         except Exception as e:
             print(e)
             print('Insertion failed!!')
         cnx.close()
+
